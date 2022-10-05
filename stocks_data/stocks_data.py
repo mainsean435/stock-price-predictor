d87f241bb0879d2879fbbaa4b7249f7abe14ddd9
mainsean435 <mainsean435@gmail.com>
2022-10-05 08:49:24 +0300

    Got rid of utils.py, all stock data logic should stay in one package :]

---
+++ stocks_data/stocks_data.py
@@ -0,0 +1,36 @@
+from distutils.command.config import config
+import pandas as pd
+from datetime import datetime
+import config
+
+
+def get_all_data(symbol):
+  filepath = f"{config.BASE_DIR}/stocks_data/{symbol}.csv"
+  df = pd.read_csv(filepath)
+
+  df = df.sort_values('Date')
+  df['Date'] = df['Date'].apply(
+      lambda x: datetime.strptime(x, r'%Y-%m-%d').timestamp())
+
+  data = dict()
+  data['open'] = df[['Date', ' Open']].values.tolist()
+  data['high'] = df[['Date', ' High']].values.tolist()
+  data['low'] = df[['Date', ' Low']].values.tolist()
+  data['close'] = df[['Date', ' Close']].values.tolist()
+  data['volume'] = df[['Date', ' Volume']].values.tolist()
+  data['previous_close'] = data['close'][-1][1]
+  data['change'] = round(data['previous_close'] - data['close'][-2][1], 3)
+  data['change_percent'] = round(
+      abs(data['change']) / data['previous_close'], 3)
+  return data
+
+
+def get_close_price(symbol):
+    filepath = f"{config.BASE_DIR}/stocks_data/{symbol}.csv"
+    df = pd.read_csv(filepath)
+
+    df = df.sort_values('Date')
+    df['Date'] = df['Date'].apply(
+        lambda x: datetime.strptime(x, r'%Y-%m-%d'))
+    return df[['Date', ' Close']]
+
