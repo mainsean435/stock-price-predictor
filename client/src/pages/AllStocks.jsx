e66384d2e962ac19a10ce7b6f870709cb7a89604
mainsean435 <mainsean435@gmail.com>
2022-10-10 03:38:33 +0300

    Add all stocks page

--- client/src/pages/AllStocks.jsx
+++ client/src/pages/AllStocks.jsx
@@ -16,21 +16,18 @@ const AllStocksPage = () => {
   }
 
   useEffect(() => {
-    let isMounted = true
     const fetchData = async () => {
       try {
-        // fetch
-        let data=""
-        if (isMounted) {
-          setStocks(data)
-        }
+        fetch("http://127.0.0.1:5000/stocks/")
+          .then((response) => response.json())
+          .then((data) => {
+            setStocks(data);
+          });
       } catch (err) {
         console.log(err);
       }
     }
     fetchData()
-
-    return () => (isMounted = false)
   }, [])
 
   const handleStockSelect = (ticker) => {
@@ -40,38 +37,38 @@ const AllStocksPage = () => {
 
   return (<div>
     <div className="text-center">
-    <table className="table hover mt-5">
-      <thead style={{ color: "rgb(79,89,102)" }}>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Last</th>
-          <th scope="col">Chg</th>
-          <th scope="col">Chg%</th>
-          <th scope="col">High</th>
-          <th scope="col">Low</th>
-          <th scope="col">Open</th>
-          <th scope="col">Pclose</th>
-        </tr>
-      </thead>
-      <tbody>
-        {stocks.map((stockData) => {
-          return (
-            <tr style={{ cursor: "pointer" }} onClick={() => handleStockSelect(stockData.ticker)} className="table-row" key={stockData.ticker}>
-              <th scope="row">{stockData.ticker}</th>
-              {/* <td>{stockData.change}</td> */}
-              <td className={`text-${changeColor(stockData.d)}`}>{stockData.change} {renderIcon(stockData.d)}</td>
-              {/* <td>{stockData.perchange}</td> */}
-              <td className={`text-${changeColor(stockData.d)}`}>{stockData.perchange} {renderIcon(stockData.d)} </td>
-              <td>{stockData.high}</td>
-              <td>{stockData.low}</td>
-              <td>{stockData.open}</td>
-              <td>{stockData.prevclose}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  </div>
+      <table className="table hover mt-5">
+        <thead style={{ color: "rgb(79,89,102)" }}>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Last</th>
+            <th scope="col">Chg</th>
+            <th scope="col">Chg%</th>
+            <th scope="col">High</th>
+            <th scope="col">Low</th>
+            <th scope="col">Open</th>
+            <th scope="col">Pclose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocks.map((stockData) => {
+            return (
+              <tr style={{ cursor: "pointer" }} onClick={() => handleStockSelect(stockData.ticker)} className="table-row" key={stockData.ticker}>
+                <th scope="row">{stockData.ticker}</th>
+                {/* <td>{stockData.change}</td> */}
+                <td className={`text-${changeColor(stockData.d)}`}>{stockData.change} {renderIcon(stockData.d)}</td>
+                {/* <td>{stockData.perchange}</td> */}
+                <td className={`text-${changeColor(stockData.d)}`}>{stockData.perchange} {renderIcon(stockData.d)} </td>
+                <td>{stockData.high}</td>
+                <td>{stockData.low}</td>
+                <td>{stockData.open}</td>
+                <td>{stockData.prevclose}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
   </div>
   )
 }
